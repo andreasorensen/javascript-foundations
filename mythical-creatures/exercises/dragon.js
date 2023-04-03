1affa20fb2e2ecff2750c873d6c0a771accd807f
Andrea Sorensen <dreasor2987@icloud.com>
2023-04-03 11:31:02 -0700

    Finish most of dragon & some of hobbit

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,35 @@
 
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
+  createDragon, 
+  greetRider, 
+  eat, 
   // findFireBreathers
 }
+
+function createDragon(name, rider, temperment) {
+  var dragon = {
+    name,
+    rider,
+    temperment,
+    timesEaten: 0,
+    hungry: true
+  }
+  return dragon
+}
+
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`
+}
+
+
+
+
+  function eat(dragon) {
+    dragon.timesEaten = dragon.timesEaten + 1;
+    if (dragon.timesEaten >= 3) {
+      dragon.hungry = false;
+    }
+    return dragon
+  }
+
--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,9 +1,28 @@
 
 
 module.exports = {
-  // createHobbit, 
-  // celebrateBirthday, 
+  createHobbit, 
+  celebrateBirthday, 
   // getRing, 
   // meetPeople, 
   // findFriends
 }
+
+function createHobbit(name = "unknown", age = 0) {
+  var hobbit = {
+    name, 
+    age
+  }
+  return hobbit
+};
+
+function celebrateBirthday(hobbit) {
+  hobbit.age = hobbit.age + 1;  
+  if (hobbit.age < 33) {
+    hobbit.isAdult = true;
+  }
+  return hobbit
+}
+
+
+
